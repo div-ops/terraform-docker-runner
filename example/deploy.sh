c6b36d65e8cbc19dffd514cc4cf3901fb158c955
creaticoding <creaticoding@gmail.com>
2021-12-31 17:19:09 +0900

    update example

--- example/deploy.sh
+++ example/deploy.sh
@@ -1,8 +1,8 @@
 echo "[deploy] start"
 
-# Docker 설치
+# Docker, git 설치
 sudo yum -y upgrade
-sudo yum -y install docker
+sudo yum -y install docker git
 
 # Docker 설치 확인
 docker -v
@@ -13,8 +13,16 @@ sudo service docker start
 # Dockerfile 가져오기
 cp /tmp/Dockerfile ./Dockerfile
 
+# 제거
+sudo docker kill $(sudo docker ps -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker rmi nextjs-runner
+
+[ -d ./creco.me/.git ] && cd creco.me && git pull origin HEAD && cd - || echo 'skip'
+[ ! -d ./creco.me/.git ] && git clone --depth=1 https://$GIT_TOKEN@github.com/creco-org/creco.me.git || echo 'skip'
+
 # 빌드
-sudo docker build . -t nextjs-runner
+sudo docker build -t nextjs-runner .
 
 # 실행
 sudo docker run -d -p 80:3000 nextjs-runner
