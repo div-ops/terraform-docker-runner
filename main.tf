7ee5f9291684a4bcb83a5bfab881e9ceefeecf78
creaticoding <creaticoding@gmail.com>
2021-12-31 17:16:25 +0900

    update example

--- main.tf
+++ main.tf
@@ -6,14 +6,20 @@ variable "region" {
   default = "ap-northeast-2"
 }
 
+variable "GIT_TOKEN" {
+  type = string
+}
+
 module "docker-single-runner" {
   source              = "git::https://github.com/div-ops/terraform-docker-runner.git//modules/docker-single-runner"
   SECURITY_GROUP_NAME = "web_security"
-  SERVICE_PORTS       = [22, 80]
+  SERVICE_PORTS       = [22, 80, 443]
   KEY_NAME            = "my-service"
   DOMAIN              = "creco.me"
   DEPLOY_SCRIPT_PATH  = "deploy.sh"
   DOCKERFILE_PATH     = "Dockerfile"
+  GIT_TOKEN           = var.GIT_TOKEN
+  VERSION             = uuid()
 }
 
 output "URL" {
